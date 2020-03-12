47e9dfba9b6363d2516c578493be51ffcb53565d
prezgamer <50046059+prezgamer@users.noreply.github.com>
2020-03-12 14:13:59 +0800

    Added in Rotating 4 spiked wall, moving rorating 4 spiked wall, shooter both left and right
    Remember to change project settings
    
    https://drive.google.com/open?id=15UcSoSSwM6qMskG-dmC_zyxY3As-c0Xh

---
+++ Scripts/Mechanic Scripts/BalloonShooter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonShooter : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletShootPos;
+
+    public float fireRate;
+
+    Animator shooterAnim;
+
+    private void Start()
+    {
+        shooterAnim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (fireRate <= 0)
+        {
+            shooterAnim.SetTrigger("Shoot");
+            fireRate = 3f; //set back to starting firerate
+        } else if (fireRate > 0)
+        {
+            fireRate -= Time.deltaTime;
+        }
+    }
+
+
+    public void ShootBullet()
+    {
+        bool shoot = false;
+
+        Instantiate(bullet, bulletShootPos.position, bulletShootPos.rotation);
+        shoot = true;
+    }
+
+}
--- Scripts/Mechanic Scripts/RopeHinge.cs
+++ Scripts/Mechanic Scripts/RopeHinge.cs
@@ -7,6 +7,11 @@ public class RopeHinge : MonoBehaviour
     HingeJoint2D ropeHinge;
     public Rigidbody2D balloonRb;
 
+    private void Start()
+    {
+        ropeHinge = GetComponent<HingeJoint2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {

c284a0c304b259edff5510f6a83a44fd5fa741b1
Terence <terresquall@hotmail.com>
2020-04-15 00:40:18 +0800

    Started coding the thread that links the balloon and crate

---
+++ Scripts/Terence/Core/Thread.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DistanceJoint2D),typeof(LineRenderer))]
+public class Thread : MonoBehaviour {
+
+    LineRenderer lineRenderer;
+    DistanceJoint2D joint;
+
+    void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+        joint = GetComponent<DistanceJoint2D>();
+    }
+
+    void Update() {
+        Debug.DrawLine((Vector2)transform.position + joint.anchor, (Vector2)transform.position + joint.connectedAnchor, Color.red);
+    }
+
+}

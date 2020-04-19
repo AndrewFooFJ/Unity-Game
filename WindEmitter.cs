ab2e3dd5b043fd26921273fbe06581c32fc2da5e
Terence <terresquall@hotmail.com>
2020-04-19 22:41:42 +0800

    Started building another balloon.

---
+++ WindEmitter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEmitter : MonoBehaviour {
+
+    ParticleSystem particles;
+    public float width = 1f;
+
+    // Start is called before the first frame update
+    void Start() {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void UpdateParticleSystem() {
+        ParticleSystem.ShapeModule shape = particles.shape;
+        shape.radius = width * 0.5f;
+    }
+}

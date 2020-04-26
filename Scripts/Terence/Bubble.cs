1e4759728a4b56506ea4135598765e6c0134520d
Terence <terresquall@hotmail.com>
2020-04-27 00:04:15 +0800

    Got a viable core mechanic.

--- Scripts/Terence/Bubble.cs
+++ Scripts/Terence/Bubble.cs
@@ -9,21 +9,22 @@ public class Bubble : MonoBehaviour {
     DistanceJoint2D joint;
     new Rigidbody2D rigidbody;
 
-    public float initialScale = 1f;
-    public float minimumScale = 0.2f;
-    public float buoyancyPerScaleUnit = 1.7f, buoyancyLossRate = 0.2f;
-    Vector3 originalScale; // Saves the original scale as point of reference.
+    // For calculating volume.
+    new Collider2D collider;
+    Vector2 originalBounds;
+    
+    public Camera targetCamera;
 
-    // Implementation for setting the scale of the object as a whole.
-    float _currentScale;
-    public float currentScale {
-        get { return _currentScale; }
-        set {
-            _currentScale = value;
-            transform.localScale = originalScale * _currentScale;
-            rigidbody.gravityScale = -_currentScale * buoyancyPerScaleUnit;
-        }
-    }
+    [Header("Buoyancy")]
+    public float volume;
+    public float minimumVolume = 0.24f, maximumVolume = 2f;
+    public float volumeLossRate = 0.03f, volumeGainRate = 0.1f;
+    public float boostVolume = 1.4f;
+
+    public enum Mode { boost, gradual }
+    public Mode controlMode;
+
+    bool isInflating = false;
 
     void Start() {
         lineRenderer = GetComponent<LineRenderer>();
@@ -31,28 +32,99 @@ public class Bubble : MonoBehaviour {
         joint = GetComponent<DistanceJoint2D>();
         rigidbody = GetComponent<Rigidbody2D>();
 
-        // Saves some scaling values.
-        originalScale = transform.localScale;
-        currentScale = initialScale;
+        collider = GetComponentInChildren<Collider2D>();
+        if(volume <= 0) volume = CalculateVolume();
+        else UpdateVolume();
+
+        // Auto-populate camera if it is missing.
+        targetCamera = targetCamera ?? Camera.main;
     }
 
     void Update() {
         lineRenderer.SetPosition(0, transform.TransformPoint(joint.anchor));
         lineRenderer.SetPosition(1, joint.connectedBody.transform.TransformPoint(joint.connectedAnchor));
 
-        if(currentScale > minimumScale) {
-            currentScale -= buoyancyLossRate * Time.deltaTime;
+        // Reduce the volume every frame by the volume loss rate.
+        if(volume > minimumVolume) volume = Mathf.Max(minimumVolume, volume - Mathf.Max(0,volumeLossRate) * Time.deltaTime);
+
+        // If it is supposed to be inflating, then inflate it.
+        if(isInflating) volume += volumeGainRate * Time.deltaTime;
+
+        UpdateVolume();
+
+        HandleInput();
+    }
+
+    void HandleInput() {
+        if(Input.GetMouseButtonDown(0)) {
+            // Handles left click down.
+            if(IsOver(Input.mousePosition)) {
+                switch(controlMode) {
+                    case Mode.boost:
+                        isInflating = false;
+                        StartCoroutine(Boost(boostVolume));
+                        break;
+                    case Mode.gradual:
+                        isInflating = true;
+                        break;
+                }
+            }
+        } else if(Input.GetMouseButtonUp(0)) {
+            // Left click up.
+            switch(controlMode) {
+                case Mode.gradual:
+                    isInflating = false;
+                    break;
+            }
         }
     }
 
-    private void OnMouseDown() {
-        StartCoroutine(Boost(0.3f));
+    // Is our balloon over a particular point on the screen?
+    bool IsOver(Vector2 screenPoint) {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition),
+                point = collider.ClosestPoint(mousePos);
+        print(mousePos + " | " + point);
+        if(point == mousePos) {
+            return true;
+        }
+        return false;
+    }
+
+    float CalculateVolume() {
+        if(collider) {
+            float vol = 4f * Mathf.PI * collider.bounds.size.x * collider.bounds.size.x * collider.bounds.size.y / 24f;
+            return vol;
+        }
+        return 0;
+    }
+
+    // Updates the scale of the object based on its volume.
+    public void UpdateVolume() {
+        if(originalBounds.sqrMagnitude <= 0)
+            originalBounds = new Vector2(
+                collider.bounds.size.x / transform.localScale.x,
+                collider.bounds.size.y / transform.localScale.y
+            );
+
+        // Limits the volume of the balloon.
+        if(volume > maximumVolume) {
+            Destroy(gameObject);
+        }
+
+        // Reverse engineers the volume equation to get the scale.
+        float scale = Mathf.Pow(
+            Mathf.Max(0,volume) / (4f * Mathf.PI * originalBounds.x * originalBounds.x * originalBounds.y / 24f),
+            1f / 3f
+        );
+
+        transform.localScale = new Vector3(scale,scale,1f);
     }
 
     // Increases the scale of the balloon over a given duration.
-    public IEnumerator Boost(float scaleAmount, float duration = 0.25f) {
+    public IEnumerator Boost(float addedVolume, float duration = 0.25f) {
         float time = 0;
         WaitForEndOfFrame w = new WaitForEndOfFrame();
+        
         while(time < duration) {
             yield return w;
 
@@ -62,7 +134,7 @@ public class Bubble : MonoBehaviour {
             if(time > duration) delta -= time - duration;
 
             // Scale up the balloon.
-            currentScale += Mathf.Lerp(0,scaleAmount,delta / duration);
+            volume += Mathf.Lerp(0,addedVolume,delta / duration);
         }
     }
 
--- WindEmitter.cs
+++ WindEmitter.cs
@@ -6,6 +6,7 @@ public class WindEmitter : MonoBehaviour {
 
     ParticleSystem particles;
     public float width = 1f;
+    public float force = 1f;
 
     // Start is called before the first frame update
     void Start() {
@@ -20,5 +21,9 @@ public class WindEmitter : MonoBehaviour {
     public void UpdateParticleSystem() {
         ParticleSystem.ShapeModule shape = particles.shape;
         shape.radius = width * 0.5f;
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        other.attachedRigidbody.AddForce(Vector2.right * force);
     }
 }
